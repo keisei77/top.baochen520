d9ccbd2088fde657bbcd0ea803e19653e8ebde06
KeiSei <keisei.jia@qq.com>
2017-02-27 23:18:43 +0800

    Init page

---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="baochen-birthday">
+    <div class="placeholder"></div>
+    <img src="../../assets/logo.png">
+    <div class="strong">I Love You!</div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="less">
+@import './index';
+</style>
